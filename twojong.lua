646617a8db8b8a01b22d23c676fb39c5da0afcdc
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2022-06-15 10:41:36 +0800

    Create twojong.lua

---
+++ twojong.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Arsenal"))()
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+local stop = Notification.new("info", "Script", "This file was customized for use of Itzbananagrilllllzzzz")
+wait(0.5)
+local stop2 = Notification.new("info", "Wehh di nga", "Have fun mate!")
